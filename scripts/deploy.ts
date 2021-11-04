9da3cd9a99f53994d26296d150c85e56ca25b37e
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2021-11-04 18:53:50 +0800

    finished Compile contract locally and run it :tada: :fire:

---
+++ scripts/deploy.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-process-exit */
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const waveContractFactory = await ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
